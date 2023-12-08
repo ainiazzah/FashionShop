eb56ecbc2c241014898bd83b98633f3336cdb2f1
zesycaanjarsari <100691245+zesycaanjarsari@users.noreply.github.com>
2023-12-08 13:29:19 +0700

    Update Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,21 +1,26 @@
 <?php
 
-namespace App\Models;
+namespace App\Models; // Menunjukkan bahwa kelas Category berada di dalam namespace App\Models
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// Mendefinisikan kelas Category yang extends (memperluas) kelas Model.
 class Category extends Model
 {
+    //Menentukan kolom-kolom pada tabel database categories yang dapat diisi secara massal 
     protected $fillable = ['shop_id', 'name', 'path'];
 
+    //Mendefinisikan relasi "many-to-one" antara Category dan Shop
     public function shop(){
         return $this->belongsTo(Shop::class);
     }
 
+    //Mendefinisikan relasi "one-to-many" antara Category dan Product.
     public function product(){
         return $this->hasMany(Product::class);
     }
 
+    // Menggunakan trait HasFactory, yang menyediakan fungsionalitas untuk membuat data pabrik
     use HasFactory;
 }
