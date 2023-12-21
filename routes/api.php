dccbbd57c7abb4aaa14ab4b440f05dad57395487
zesycaanjarsari <100691245+zesycaanjarsari@users.noreply.github.com>
2023-12-21 13:16:27 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\Route;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| Di sinilah Anda dapat mendaftarkan rute API untuk aplikasi Anda. Ini
+| rute dimuat oleh RouteServiceProvider dalam grup yang
+| ditugaskan ke grup middleware "api". Selamat membangun API Anda!
 |
 */
 
